2f9d58ddb5a54906f5ea1af913f13477e672c5aa
Manash <manash@cefalo.com>
2019-06-14 17:46:07 +0600

    Add form component

---
+++ src/components/form/Form.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Form extends Component {
+    render() {
+        return (
+            <form>
+                <input type="text" name="name" />
+            </form>
+        );
+    }
+}
+
+export default Form;
