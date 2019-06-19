1a69a0bdd3dd96c3300f1fba55f08e184358650f
Manash <manash@cefalo.com>
2019-06-19 11:12:53 +0600

    Multiple textinput field

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,11 +1,46 @@
 import React, {Component} from 'react';
+import TextInput from './TextInput';
 
 class Form extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            formControls: {
+                name: {
+                    value: ''
+                    , placeholder: 'What is your name'
+                }
+                , amount: {
+                    value: ''
+                    , placeholder: 'How much do you want'
+                }
+            }
+        }
+    }
+
+    changeHandler = event => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        this.setState(
+            {
+                formControls: {
+                    ...this.state.formControls
+                    , [name]: {
+                        ...this.state.formControls[name]
+                        , value
+                    }
+                }
+            }
+        );
+    }
+
     render() {
         return (
-            <form>
-                <input type="text" name="name" />
-            </form>
+            <div>
+                <TextInput name="name" placeholder= {this.state.formControls.name.placeholder} value={this.state.formControls.name.value} onChange={this.changeHandler}/>
+                <TextInput name="amount" placeholder= {this.state.formControls.amount.placeholder} value={this.state.formControls.amount.value} onChange={this.changeHandler}/>
+            </div>
         );
     }
 }
